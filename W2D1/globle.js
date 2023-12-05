b857ed79688ad9bb5075ade2ce445ec5a477fe29
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-12-05 09:59:56 -0600

    feat: add lap w2d1

--- W2D1/globle.js
+++ W2D1/globle.js
@@ -4,7 +4,6 @@
 // console.log(screen);
 // console.log(location);
 
-
 // document.write("Hello World");
 
 // console.log(str)
@@ -13,9 +12,48 @@
 // }
 // console.log(str);
 
-// Print 
+// Print
 // undefined
 // Hello World
 
+// x = 1;
+// var a = 5;
+// var b = 10;
+// var c = function (a, b, c) {
+//   document.write(x);
+//   document.write(a);
+//   var f = function (a, b, c) {
+//     b = a;
+//     document.write(b);
+//     b = c;
+//     var x = 5;
+//   };
+//   f(a, b, c);
+//   document.write(b);
+//   var x = 10;
+// };
+// c(8, 9, 10);
+// document.write(b);
+// document.write(x);
 
+var x = 9;
 
+function myFunction() {
+return x * x;
+
+}
+
+document.write (myFunction());
+
+x = 5;
+
+document.write (myFunction());
+
+// var foo = 1;
+// function bar() {
+//   if (!foo) {
+//     var foo = 10;
+//   }
+//   alert(foo);
+// }
+// bar();
