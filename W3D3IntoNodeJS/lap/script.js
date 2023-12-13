fa1bbfe9f5f0a899f31fe895c5e3e32518c779f8
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-12-13 15:05:27 -0600

    Change Conflict Naame In Lap

--- W3D3IntoNodeJS/lap/script.js
+++ W3D3IntoNodeJS/lap/script.js
@@ -12,7 +12,7 @@ Array.prototype.odd = function () {
     return this.filter((number) => number % 2 !== 0);
 }
 
-console.log([1, 2, 3, 4, 5, 6, 7, 8].even());
-console.log([1, 2, 3, 4, 5, 6, 7, 8].odd());
+console.log([1, 2, 3, 4, 5, 6, 7, 8].even()); // [2,4,6,8]
+console.log([1, 2, 3, 4, 5, 6, 7, 8].odd()); // [1,3,5,7]
 
 
